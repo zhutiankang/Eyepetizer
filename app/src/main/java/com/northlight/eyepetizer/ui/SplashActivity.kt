119a7ce177647b1a31574b66a84b39de75488ef0
tiankang <ztk1993@163.com>
2017-12-06 17:57:46 +0800

    启动页面实现

--- app/src/main/java/com/northlight/eyepetizer/ui/SplashActivity.kt
+++ app/src/main/java/com/northlight/eyepetizer/ui/SplashActivity.kt
@@ -1,8 +1,18 @@
 package com.northlight.eyepetizer.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import com.northlight.eyepetizer.R
+import com.northlight.eyepetizer.utils.newIntent
+import kotlinx.android.synthetic.main.activity_splash.*
 
 /**
  * author : 祝天康
@@ -11,7 +21,52 @@ import android.support.v7.app.AppCompatActivity
  */
 class SplashActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private lateinit var tv_name : TextView
+    private lateinit var tv_intro : TextView
+    private lateinit var image_bg : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //设置全屏
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+        initView()
+    }
+
+    private fun initView() {
+        tv_name = tv_name_english
+        tv_intro = tv_intro_english
+        image_bg = iv_icon_splash
+
+        val font :Typeface = Typeface.createFromAsset(this.assets,"fonts/Lobster-1.4.otf")
+        tv_name.typeface = font
+        tv_intro.typeface = font
+
+        val alphaAnimation = AlphaAnimation(0.1f,1.0f)
+        alphaAnimation.duration = 1000
+
+        val scaleAnimation = ScaleAnimation(0.1f,1.0f,0.1f,1.0f,
+                ScaleAnimation.RELATIVE_TO_SELF,0.5f,
+                ScaleAnimation.RELATIVE_TO_SELF,0.5f)
+        scaleAnimation.duration = 1000
+
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(alphaAnimation)
+        animationSet.addAnimation(scaleAnimation)
+        animationSet.duration = 1000
+
+        image_bg.startAnimation(animationSet)
+        animationSet.setAnimationListener(object :Animation.AnimationListener{
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                newIntent<MainActivity>()
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+            }
+        })
+
+
     }
 }
--- app/src/main/java/com/northlight/eyepetizer/utils/Extension.kt
+++ app/src/main/java/com/northlight/eyepetizer/utils/Extension.kt
@@ -1,6 +1,8 @@
 package com.northlight.eyepetizer.utils
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 /**
@@ -10,4 +12,9 @@ import android.widget.Toast
  */
 fun Context.showToast(message: String){
     Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
+}
+
+inline fun<reified T: Activity> Activity.newIntent() {
+    val intent = Intent(this, T::class.java)
+    startActivity(intent)
 }
